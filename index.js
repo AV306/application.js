3b9ca19b125d86f3eed4ff393c59dfc8e3d17ef0
AV3_08 <66512356+AV306@users.noreply.github.com>
2021-09-15 14:42:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 
 // Read commands
-client.commands = new Collection();
+const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	// Set a new item in the Collection of commands with the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
+	commands.push(command.data.toJSON());
 }
+
+
+// Place your client and guild ids here
+const clientId = '';
+const guildId = '';
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+	try {
+		console.log('Attempting ro refresh application slash commands...');
+
+		await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands }); // COMMENT ONLY ON PRODUCTION
+		// await rest.put(Routes.applicationCommands(clientId), { body: commands }); // UNCOMMENT ONLY ON PRODUCTION
+
+		console.log('Successfully refreshed application slash commands.');
+	} catch (error) { console.error(error); }
+})();
 
 
 // Read events
