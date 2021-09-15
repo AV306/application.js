eae7d4ce068e0a0e4ae62baf8e91995736f86b1f
AV3_08 <66512356+AV306@users.noreply.github.com>
2021-09-15 14:27:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,41 +4,29 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+
+// Read commands
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-  
-	// Set a new item in the Collection of commands
-	// With the key as the command name and the value as the exported module
+	// Set a new item in the Collection of commands with the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
 }
 
 
+// Read events
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
-client.once('ready', () => {console.log("Ready!");}); // print "Ready!" once bot is ready
 
-// on interact...
-client.on('interactionCreate', async interaction => {
-	// Log interaction
-	console.log('${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.');
-	
-	
-	
-	// Handle interation
-	if (!interaction.isCommand()) return; // if it's not a command, ignore
-	const command = client.commands.get(interaction.commandName); // get the command from Collection
-	if (!command) return; // ignore if its not a valid command
-	try {
-		await command.execute(interaction); // await execution of response
-	} 
-    catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-	
-});
 
 
 client.login(token); // login
