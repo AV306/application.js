e0cd74f67d01448146d89c567d402d82627790a3
AV3_08 <66512356+AV306@users.noreply.github.com>
2021-09-16 08:46:50 +0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,3 +10,19 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+	try {
+		console.log("Attempting to refresh commands...");
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully refreshed application commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
