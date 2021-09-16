2be1357bcd27cedc53064342a09ed6917d07cb29
AV3_08 <66512356+AV306@users.noreply.github.com>
2021-09-16 08:55:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,44 +7,44 @@ const { token } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 
-// Read commands
-const commands = [];
+
+
+
+client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+	client.commands.set(command.data.name, command);
 }
 
 
-// Place your client and guild ids here
-const clientId = '';
-const guildId = '';
 
-const rest = new REST({ version: '9' }).setToken(token);
 
-(async () => {
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
 	try {
-		console.log('Attempting ro refresh application slash commands...');
-
-		await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands }); // COMMENT ONLY ON PRODUCTION
-		// await rest.put(Routes.applicationCommands(clientId), { body: commands }); // UNCOMMENT ONLY ON PRODUCTION
-
-		console.log('Successfully refreshed application slash commands.');
-	} catch (error) { console.error(error); }
-})();
-
-
-// Read events
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+		console.log(`${interaction.user}` + " executed " + command + "!");
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
-}
+});
+
+
+
 
 
 
